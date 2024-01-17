ca6ea48ab90495fa1888e993065ee593abd4957e
Amir03209 <ah03209120917>
2024-01-17 05:11:39 -0800

    header completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 const App = () => {
   return (
-    <div className='text-red-900 m-10'>
-      App
-    </div>
-  )
-}
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
