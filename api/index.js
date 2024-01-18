86e59b7f8ad6a019878b230bd5df6b0fb8bc2d44
Amir03209 <ah03209120917>
2024-01-17 21:55:54 -0800

    Database Successfully Connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connected To MongoDb");
+}).catch((error)=>{
+    console.log(error)
+})
 const app= express();
 app.listen(3000,()=>{
     console.log('Port is running on port 3000')
