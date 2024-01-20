09170ab488140afef5a08a9948b3d8c99f621b78
Amir03209 <ah03209120917>
2024-01-19 22:58:29 -0800

    signin functionlity completed

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,15 +1,34 @@
-import User from '../models/user.model.js'
-import bcryptjs from 'bcryptjs'
-export const signUp = async (req,res,next)=>{
-const {username,email,password}=req.body;
-const hashedPassword = bcryptjs.hashSync(password,10);
-const newUser = new User({username,email,password:hashedPassword});
-try {
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+export const signUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
-    res.status(201).json('User Created successfully')
-} catch (error) {
+    res.status(201).json("User Created successfully");
+  } catch (error) {
     // res.status(500).json(error.message)
-    next(error)
-}
+    next(error);
+  }
+};
 
-}
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
 const SignIn = () => {
+  const navigate = useNavigate();
+  const [formData,setFormData]=useState({})
+  const [isLoading,setIsLoading]=useState(false)
+  const [error,setError]=useState(null)
+  const handleChange=(e)=>{
+    setFormData({
+    ...formData,
+    [e.target.id]:e.target.value
+    })
+  }
+  const handleSubmit = async (e)=>{
+   e.preventDefault()
+   setIsLoading(true)
+   const res = await fetch('/api/auth/signin',{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body : JSON.stringify(formData)
+   })
+   const data = await res.json()
+   if(data.success===false){
+    setIsLoading(false)
+    setError(data.message)
+    return
+   }
+   setIsLoading(false)
+   setError(null)
+   navigate('/home')
+   console.log(data)
+  }
+  
+  console.log(formData)
   return (
-    <div>
-      Sign In
-    </div>
-  )
-}
+    <div className="max-w-lg mx-auto p-3">
+      <h1 className="text-3xl font-bold text-center my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="p-3 border rounded-lg"
+          onChange={handleChange}
 
-export default SignIn
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="p-3 border rounded-lg"
+          onChange={handleChange}
+
+        />
+        <button disabled={isLoading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {isLoading ? "Loading" : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Do not have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700 ">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-800 mt-5 px-1">{error}</p> }
+    </div>
+  );
+};
+
+export default SignIn;
+
