790ebe551e3f8de1655b98df7db60d62dec641af
Amir03209 <ah03209120917>
2024-01-28 22:11:09 -0800

    created get user listings api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
  import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
@@ -42,4 +43,18 @@ export const deleteUser = async (req,res,next)=>{
     } catch (error) {
       next(error)
     }
+}
+
+export const getUserListings = async (req,res,next)=>{
+ if(req.user.id===req.params.id){
+ try {
+  const listings = await Listing.find({userRef : req.user.id})
+  res.status(200).json(listings)
+ } catch (error) {
+  next(error)
+ }
+ }
+ else{
+  next(errorHandler(401,"You can view your own listing only!"))
+ }
 }
