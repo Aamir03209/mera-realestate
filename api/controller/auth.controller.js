5bacb1d5ee5eaad936c2046934dc6453ee74e9bb
Amir03209 <ah03209120917>
2024-01-30 21:56:17 -0800

    created search api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -34,21 +34,17 @@ export const signIn = async (req, res, next) => {
 };
 
 export const google = async (req, res, next) => {
- 
   try {
-    const user = await User.findOne({ email : req.body.email });
+    const user = await User.findOne({ email: req.body.email });
     if (user) {
-    
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
-      
-     
+
       res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
         .json(rest);
     } else {
-      
       const generatePassword =
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
@@ -57,16 +53,15 @@ export const google = async (req, res, next) => {
         username:
           req.body.name.split(" ").join("").toLowerCase() +
           Math.random().toString(36).slice(-8),
-          email: req.body.email,
-          password: hashedPassword,
-          avatar: req.body.photo,
-          
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
       });
 
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
-     
+
       res
         .cookie("access_token", token, { httpOnly: true })
         .status(200)
@@ -77,11 +72,12 @@ export const google = async (req, res, next) => {
   }
 };
 
-export const signout = (req,res,next)=>{
+export const signout = (req, res, next) => {
   try {
-     res.clearCookie('access_token');
-     res.status(200).json("You have successfully logged out")
+    res.clearCookie("access_token");
+    res.status(200).json("You have successfully logged out");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
