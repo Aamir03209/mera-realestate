f9068fa36ebec83bc177aa6ec5bdbb14cf439348
Amir03209 <ah03209120917>
2024-01-31 22:17:34 -0800

    pre installation process

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js'
+import path from 'path';
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app= express();
 app.use(express.json())
 app.use(cookieParser())
@@ -25,6 +27,11 @@ app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listingRouter)
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 //Middleware
 
